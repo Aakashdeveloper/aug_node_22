a549d4b8b9cf4c9054e3e3718e5fdabb90be403b
aakashdeveloper <ahanda205@gmail.com>
2022-09-09 09:50:57 +0530

    zomapi

--- zomatoapi/apidocs.md
+++ zomatoapi/apidocs.md
@@ -1,13 +1,18 @@
 // Page1
 
 # List of city
+> http://localhost:9100/location
 # List of restaurant
+> http://localhost:9100/restaurants
 # Restaurants wrt city
+> http://localhost:9100/restaurants?stateId=3
 # MealType
+> http://localhost:9100/mealType
 
 //Page2
 
 # Restaurants wrt MealType
+> 
 # Filter on basis of cuisine
 # Filter on basis of cost
 # Sort on basis of Price
--- zomatoapi/app.js
+++ zomatoapi/app.js
@@ -1,6 +1,7 @@
 let express = require('express');
 let app = express();
 let mongo = require('mongodb');
+let MongoClient = mongo.MongoClient;
 let cors = require('cors');
 let bodyParser = require('body-parser');
 let dotenv = require('dotenv');
@@ -8,11 +9,19 @@ dotenv.config()
 let Port = process.env.PORT || 7100;
 let mongoUrl = process.env.mongoLiveUrl;
 let db;
-
+let authKey = process.env.authKey;
 // middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
+
+function auth(key){
+    if(key == authKey){
+        return true
+    }else{
+        return false
+    }
+}
 
 // heartbeat
 app.get('/',(req,res) => {
@@ -20,11 +29,50 @@ app.get('/',(req,res) => {
 })
 
 
+// list of City
+app.get('/location',(req,res) => {
+    //let key = req.query.key;
+    //let {key} = req.query;
+    let key = req.header('x-basic-token')
+    if(auth(key)){
+        db.collection('location').find().toArray((err,data) => {
+            if(err) throw err;
+            res.send(data)
+        })
+    }else{
+        res.send('Unauthenticated Requested')
+    }
+})
+
+//restaurant
+app.get('/restaurants',(req,res) => {
+    let query = {}
+    let stateId = Number(req.query.stateId)
+    if(stateId){
+        query = {'state_id':stateId}
+    }
+    db.collection('restaurants').find(query).toArray((err,data) => {
+        if(err) throw err;
+        res.send(data)
+    })
+})
+
+//mealType
+app.get('/mealType',(req,res) => {
+    db.collection('mealType').find().toArray((err,data) => {
+        if(err) throw err;
+        res.send(data)
+    })
+})
+
+
 
 MongoClient.connect(mongoUrl,(err,client)=>{
     if(err) console.log('Error While Connecting')
     db = client.db('augintern')
-    app.listen(port,()=>{
-        console.log(`Listing to port ${port}`)
+    app.listen(Port,()=>{
+        console.log(`Listing to port ${Port}`)
     })
 })
+
+
